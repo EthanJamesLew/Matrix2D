1ed708001d5e8c01354daf3dfd493fb887c91f8a
EthanJamesLew <ethan.lew@pcc.edu>
2016-02-15 15:03:17 -0800

    Added identity() and permutative() methods. Comments added.

--- Math/matrix2d.h
+++ Math/matrix2d.h
@@ -14,7 +14,7 @@ template <typename T> class Matrix2D {
   Matrix2D(const Matrix2D<T>& rhs);
   virtual ~Matrix2D();
 
-                                             
+  //Arithmetic matrix-matrix operations                                           
   Matrix2D<T>& operator=(const Matrix2D<T>& rhs);                                              
   Matrix2D<T> operator+(const Matrix2D<T>& rhs);
   Matrix2D<T>& operator+=(const Matrix2D<T>& rhs);
@@ -22,24 +22,29 @@ template <typename T> class Matrix2D {
   Matrix2D<T>& operator-=(const Matrix2D<T>& rhs);
   Matrix2D<T> operator*(const Matrix2D<T>& rhs);
   Matrix2D<T>& operator*=(const Matrix2D<T>& rhs);
+
+  //Square matrix properties
   Matrix2D<T> transpose();
-                                     
+  Matrix2D<T> identity();
+  Matrix2D<T> permutative();
+
+  //Arithmetic scalar-matrix operations                                   
   Matrix2D<T> operator+(const T& rhs);
   Matrix2D<T> operator-(const T& rhs);
   Matrix2D<T> operator*(const T& rhs);
   Matrix2D<T> operator/(const T& rhs);
-
-                                           
+                                      
+  //Arithmetic vector-matrix operations
   std::vector<T> operator*(const std::vector<T>& rhs);
   std::vector<T> diag_vec();
-
                                              
   T& operator()(const unsigned& row, const unsigned& col);
   const T& operator()(const unsigned& row, const unsigned& col) const;
-
-                                              
+                                         
   unsigned get_rows() const;
   unsigned get_cols() const;
+
+  bool isSquare();
 
 };
 
